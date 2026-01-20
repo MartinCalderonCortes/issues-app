8cee97f663a962500709f6c94993da7ff60bca01
Martin <mh.straus@hotmail.com>
2026-01-20 17:29:32 -0500

    display the github labes on the LabelPicker section

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,12 +1,29 @@
+import { useLabels } from "../hooks/useLabels";
+
 export const LabelPicker = () => {
+  const { labelsQuery } = useLabels()
+
+  if( labelsQuery.isLoading ) {
+    return (
+      <div className="flex justify-center items-center h-52">
+        loading...
+      </div>
+    )
+  }
+  
   return (
-    <>
-      <span
-        className="px-2 py-1 rounded-full text-xs font-semibold hover:bg-slate-800 cursor-pointer"
-        style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
-      >
-        Primary
-      </span>
-    </>
+    <div className="flex flex-wrap gap-2 justify-start">
+      {
+        labelsQuery.data && labelsQuery.data.map(({ id, name, color }) => (
+          <span
+            className="animate-fadeIn px-2 py-1 rounded-full text-xs font-semibold hover:bg-slate-800 cursor-pointer"
+            style={{ border: `1px solid #${color}`, color: '#ffccd3' }}
+            key={id}
+          >
+            {name}
+          </span>
+        ))
+      }
+    </div>
   );
 };
