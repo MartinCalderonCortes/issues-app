9b8f2c2fbc4a63978813d24749070eadeea67b9e
Martin <mh.straus@hotmail.com>
2026-01-23 14:34:56 -0500

    Implement the loading spinner component

--- src/share/components/LoadingSpinner.tsx
+++ src/share/components/LoadingSpinner.tsx
@@ -2,10 +2,8 @@ import { FiRefreshCcw } from "react-icons/fi"
 
 export const LoadingSpinner = () => {
     return (
-        // <div className="loading">
-            <svg className="animate-spin">
-                <FiRefreshCcw size={20}/>
-            </svg>  
-        // </div>
+        <div className="flex flex-row w-full h-52 justify-center items-center">
+            <FiRefreshCcw size={40} className="animate-spin" />
+        </div>
     )
 }
